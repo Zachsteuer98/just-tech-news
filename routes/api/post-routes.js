1a2fd238ba48a99827d222fb57c58b82ee59a329
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-02-24 15:27:58 -0500

    added comments, launching to heroku next

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -15,6 +15,21 @@ router.get('/', (req, res) => {
             model: User,
             attributes: ['username']
             }
+        ],
+        include: [
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
         ]
     })
     .then(dbPostData => res.json(dbPostData))
